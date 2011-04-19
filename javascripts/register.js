afa1da8c86608a7a88893e5785739bb41c4aab81
Alex Hokanson <avidgamer123@gmail.com>
2011-04-19 19:48:32 -0400

    changed javascript to use DOM getElementById method

--- javascripts/register.js
+++ javascripts/register.js
@@ -1,36 +1,45 @@
 function isValid(form) {
 	
 	//get all of the elements from the form
-	var firstName = form.elements["firstName"];
-	var lastName = form.elements["lastName"];
-	var email1 = form.elements["email1"];
-	var email2 = form.elements["email2"];
-	var currentStanding = form.elements["currentStanding"];
-	var bdayMonth = form.elements["bdayMonth"];
-	var bdayDay = form.elements["bdayDay"];
-	var bdayYear = form.elements["bdayYear"];
-	var username = form.elements["username"];
-	var password1 = form.elements["password1"];
-	var password2 = form.elements["password2"];
+	var firstName = document.getElementById('firstName');
+	var lastName = document.getElementById("lastName");
+	var email1 = document.getElementById("email1");
+	var email2 = document.getElementById("email2");
+	var currentStanding = document.getElementById("currentStanding");
+	var bdayMonth = form.elements["bdayMonth");
+	var bdayDay = document.getElementById("bdayDay");
+	var bdayYear = document.getElementById("bdayYear");
+	var username = document.getElementById("username");
+	var password1 = document.getElementById("password1");
+	var password2 = document.getElementById("password2");
 	
 	//make sure everything is filled out
 	if (firstName.value == "" || lastName.value == "" || email1.value == "" || email2.value == "" || currentStanding.value == "" || bdayMonth.value == "" || bdayDay.value == "" || bdayYear.value == "" || username.value == "" || password1.value == "" || password2.value == "") {
-		alert("Please fill in all fields.<br/>");
+		alert("Please fill in all fields.");
 		return false;
 	}
 	
 	//check if the passwords are the same
 	if (form.password1.value == form.password2.value) {}
 	else {
-		alert("Please make sure your passwords match.<br/>");
+		alert("Please make sure your passwords match.");
 		return false;
 	}
 	//check if the emails are the same
 	if (form.email1.value == form.email2.value) {}
 	else {
-		alert("Please make sure your emails match.<br/>");
+		alert("Please make sure your emails match.");
 		return false;
 	}
+	
+	//make sure they are using their Manchester email account.  
+	//it must end in ...spartans.manchester.edu or ...manchester.edu
+	var pattern = /manchester.edu$/;
+	if(!pattern.test(email1.value)) {
+		alert("You must use your Manchester email.");
+		return false;
+	}
+	
 	//everything has been checked so we can go on to send the data to the database
 	return true;
 }
--- register.php
+++ register.php
@@ -26,6 +26,54 @@
 				$query = "INSERT INTO Users VALUES ('$first','$last','".bdayYear."/".bdayMonth."/".bdayDay."','$currentStanding','$username','$password','$random','0','$email','DEFAULT','DEFAULT','$random','0');";
 				$result = mysql_query($query) or die("Query failed: " . mysql_error() . "<br>");
 
+
+				/*echo "starting mail sending";
+				//copy
+				
+				require_once "/home/alex/pear/share/pear/Mail.php";
+				echo "1";
+        		$from = "<verify.impressions@gmail.com>";
+       		 	$to = "<avidgamer123@gmail.com>";
+        		$subject = "Activate your account";
+        		$body = "
+				\n
+				Hello $firstName,
+				\n
+				\n
+				Please copy/paste the following URL into your browser to activate your account:
+				\n
+				10.40.40.84/activate.php?username=$username&code=$random;
+				\n
+				\n
+				Thanks!
+				";
+
+        		$host = "ssl://smtp.gmail.com";
+        		$port = "465";
+        		$username = "verify.impressions@gmail.com";
+        		$password = "!mpressions";
+echo "2";
+        		$headers = array ('From' => $from,
+          			'To' => $to,
+          			'Subject' => $subject);
+          		echo "3";
+        		$smtp = Mail::factory('smtp',
+          		array ('host' => $host,
+            		'port' => $port,
+            		'auth' => true,
+            		'username' => $username,
+            		'password' => $password));
+echo "4";
+        		$mail = $smtp->send($to, $headers, $body);  //this is where it stops... it prints out 4, but not 5
+echo "5";
+				if (PEAR::isError($mail)) {
+					echo("<p>" . $mail->getMessage() . "</p>");
+				} else {
+					echo("<p>Message successfully sent!</p>");
+				}
+				
+				//copy end
+				echo "mail sent hopefully.";
 				//sending email
 				$to = $email;
 				$subject = "Activate your account";
@@ -48,7 +96,7 @@
 				";
 
 				//function to send email
-				mail($to, $subject, $body, $headers);
+				mail($to, $subject, $body, $headers);*/
 				//Close the connection to the server since we're done querying
 				mysql_close($link);
 				echo "You have successfully registered with Impressions!</br>Please check your e-mail for a verification e-mail.";
